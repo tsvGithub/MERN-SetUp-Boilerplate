a709ac14e15f544af89129d2067a1b2c35894590
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-09-28 10:23:55 +0300

    model update

--- models/Something.model.js
+++ models/Something.model.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const somethingSchema = new Schema(
   {
-    memo: { type: String, required: true },
-    username: { type: String, required: true, unique: true, trim: true, minlength: 3 },
-    importance: { type: String, required: true },
-    area: { type: String, required: true },
-    salary: { type: Number, required: true },
+    uno: { type: String, required: true },
+    dos: { type: String, required: true, unique: true, trim: true, minlength: 3 },
+    tres: { type: String, required: true },
+    cuatro: { type: String, required: true },
+    cinco: { type: Number, required: true },
     date: { type: Date, required: true },
   },
   {
